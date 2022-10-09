3af939d1b64ffa4222310be4fee68ab79d1158b3
bot-l <55227755+bot-l@users.noreply.github.com>
2022-10-09 11:36:55 +0700

    Update t.sh

--- t.sh
+++ t.sh
@@ -1,9 +1,9 @@
-apt -y update; apt -y install screen git nodejs
-pkg install nodejs -y
-pkg install nodejs-lts -y
+yum -y update; yum -y install screen git nodejs
+yum install nodejs -y
+yum install nodejs-lts -y
 npm i fake-useragent
 npm i randomstring
 npm i request
-pkg install python3 -y
+yum install python3 -y
 pip3 install -r requirements.txt
 python3 F-Tool.py
